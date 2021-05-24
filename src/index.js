280b785d3805cf35a90153f9c2883cc2e1e71592
thealan404 <gokdenizbektas@gmail.com>
2021-05-24 14:31:26 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // Life.js
 
+const PersonCollection = require("./PersonCollection.js");
+
 module.exports = {
   Person: require("./Person.js"),
+  PersonCollection: ,
+  Persons: PersonCollection.generate(),
   Space: require("./Space.js"),
   World: require("./World.js"),
 };
